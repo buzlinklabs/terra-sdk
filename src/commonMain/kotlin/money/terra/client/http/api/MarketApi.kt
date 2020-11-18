22544fffd84527fdf78110b3d404ea2a458dc1b7
Jade Kim <jinyong@jadekim.kr>
2020-11-18 21:53:52 +0900

    Fix estimateSwapResult api error

--- src/commonMain/kotlin/money/terra/client/http/api/MarketApi.kt
+++ src/commonMain/kotlin/money/terra/client/http/api/MarketApi.kt
@@ -3,16 +3,12 @@ package money.terra.client.http.api
 import money.terra.client.http.TerraHttpClient
 import money.terra.model.Coin
 import money.terra.model.ResultWrapper
-import money.terra.model.Transaction
-import money.terra.model.TypeWrapper
-import money.terra.model.bank.SendCoinRequest
-import money.terra.transaction.message.SendMessage
 
 class MarketApi(
     private val client: TerraHttpClient
 ) {
 
-    suspend fun estimateSwapResult(denom: String, amount: String): ResultWrapper<List<Coin>> {
-        return client.get("/market/swap", mapOf("denom" to denom, "amount" to amount))
+    suspend fun estimateSwapResult(quantity: String, offerDenom: String, askDenom: String): ResultWrapper<Coin> {
+        return client.get("/market/swap", mapOf("offer_coin" to quantity + offerDenom, "ask_denom" to askDenom))
     }
 }
